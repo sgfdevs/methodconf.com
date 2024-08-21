1448f0679eaa9391c1cf836055e58dba8a262449
Levi Zitting <me@levizitting.com>
2024-08-20 22:18:49 -0500

    feat: update ticket url

---
+++ frontend/src/app/tickets/route.ts
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export function GET() {
+    redirect('https://l.oveit.com/events/embed?id=5b12c452dc');
+}
--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -2,4 +2,4 @@ export const SPEAKER_LINK = 'https://sessionize.com/method-conference-2024/';
 export const LOCATION_IFRAME_URL =
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3177.5064710178376!2d-93.29300142284976!3d37.211950144379166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87cf62fce4b64373%3A0xb83189288308d87f!2sefactory!5e0!3m2!1sen!2sus!4v1717465582324!5m2!1sen!2sus';
 
-export const TICKET_LINK = 'https://l.oveit.com/events/embed?id=5b12c452dc';
+export const TICKET_LINK = '/tickets';
