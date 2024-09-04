cc78872b9339f41066762fdfc9b477a2ec4d569f
Levi Zitting <me@levizitting.com>
2024-09-04 07:24:06 -0500

    feat: session grid generation

--- frontend/src/components/sections/ScheduleSection.tsx
+++ frontend/src/components/sections/ScheduleSection.tsx
@@ -1,11 +1,6 @@
 import { SectionTitleBar } from '@/components/SectionTitleBar';
 import { getSchedule } from '@/data/getSchedule';
-import {
-    ScheduleItem,
-    SessionWithDates,
-    Track,
-    TrackWithSessions,
-} from '@/data/types';
+import { ScheduleItem, SessionWithDates } from '@/data/types';
 import { splitBy } from '@/util';
 import { getConference } from '@/data/getConference';
 
@@ -15,6 +10,50 @@ export async function ScheduleSection() {
     const conference = await getConference();
     const schedule = await getSchedule(conference!.id);
 
+    const tracks = schedule.filter((item) => item.contentType === 'track');
+
+    const cssGrid: string[][] = createSessionGrid(schedule).map((gridRow) =>
+        gridRow.map((gridColumn) =>
+            gridColumn ? gridColumn.route.path : 'none',
+        ),
+    );
+
+    return (
+        <section>
+            <SectionTitleBar title="Schedule" />
+
+            <div>
+                {tracks.map((track) => (
+                    <p key={track.id}>{track.name}</p>
+                ))}
+            </div>
+
+            <pre>
+                {cssGrid
+                    .map((row) => row.map((col) => col.padEnd(100)).join(' '))
+                    .join('\n')}
+            </pre>
+        </section>
+    );
+}
+
+function getSessionDuration(session: SessionWithDates): number {
+    return (
+        (session.properties.end?.getTime() ?? 0) -
+        (session.properties.start?.getTime() ?? 0)
+    );
+}
+
+function sessionSort(a?: SessionWithDates, b?: SessionWithDates): number {
+    return (
+        (a?.properties.start?.getTime() ?? 0) -
+        (b?.properties.start?.getTime() ?? 0)
+    );
+}
+
+function createSessionGrid(
+    schedule: ScheduleItem[],
+): (SessionWithDates | null)[][] {
     const [tracks, topLevelSessions] = splitBy(
         schedule,
         (item) => item.contentType === 'track',
@@ -48,13 +87,13 @@ export async function ScheduleSection() {
 
     const totalRows = mostBlocksInTrack + topLevelSessions.length;
 
-    const grid: string[][] = [];
+    const sessionGrid: (SessionWithDates | null)[][] = [];
 
     let topLevelSessionIndex = 0;
     let trackSessionIndex = 0;
 
     for (let i = 0; i < totalRows; i++) {
-        grid.push([]);
+        sessionGrid.push([]);
 
         const topLevelSession = topLevelSessions[topLevelSessionIndex];
         const rowTrackSession = trackSessions[trackSessionIndex];
@@ -65,7 +104,7 @@ export async function ScheduleSection() {
 
         if (isTopLevelSessionRow) {
             for (let j = 0; j < trackCount; j++) {
-                grid[i]![j] = topLevelSession?.route.path ?? 'none';
+                sessionGrid[i]![j] = topLevelSession;
             }
 
             topLevelSessionIndex++;
@@ -99,101 +138,46 @@ export async function ScheduleSection() {
             );
 
             if (rowTrackSession) {
-                grid[i]![j] = rowTrackSession.route.path;
+                sessionGrid[i]![j] = rowTrackSession;
                 continue;
             }
 
-            grid[i]![j] = 'none';
+            let previousSessionSearchDepth = 1;
+            const previousRowSession =
+                sessionGrid[i - previousSessionSearchDepth]?.[j];
+
+            if (previousRowSession) {
+                while (true) {
+                    previousSessionSearchDepth++;
+
+                    const beforePreviousRowSession =
+                        sessionGrid[i - previousSessionSearchDepth]?.[j];
+
+                    if (!beforePreviousRowSession) {
+                        break;
+                    }
+
+                    if (beforePreviousRowSession.id !== previousRowSession.id) {
+                        break;
+                    }
+                }
+
+                const neededSessionBlocks = Math.trunc(
+                    getSessionDuration(previousRowSession) /
+                        minimumSessionDuration,
+                );
+
+                const renderedSessionBlocks = previousSessionSearchDepth - 1;
+
+                if (neededSessionBlocks > renderedSessionBlocks) {
+                    sessionGrid[i]![j] = previousRowSession;
+                    continue;
+                }
+            }
+
+            sessionGrid[i]![j] = null;
         }
     }
 
-    // const trackWithMostSessions = tracks.reduce((most, track) => {
-    //     return most.children.length > track.children.length ? track : most;
-    // });
-    //
-    // const tracksWithEmptySessions: TrackWithEmptySessions[] = tracks.map(
-    //     (track) => {
-    //         for (let i = 0; i < trackWithMostSessions.children.length; i++) {
-    //             const templateSession = trackWithMostSessions.children[i];
-    //             const session = track.children[i];
-    //         }
-    //     },
-    // );
-
-    // for (const item of schedule) {
-    //     if (item.contentType !== 'track') {
-    //         continue;
-    //     }
-    //
-    //     item.children.sort(sessionSort);
-    // }
-
-    // const tracksWithAllSessions = tracks.map((track) => {
-    //     track.children.push(...topLevelSessions);
-    //     track.children.sort(sessionSort);
-    //     return track;
-    // });
-    //
-    // const trackWithMostSessions = tracksWithAllSessions.reduce(
-    //     (most, track) => {
-    //         return most.children.length > track.children.length ? most : track;
-    //     },
-    // );
-    //
-    // for (const track of tracksWithAllSessions) {
-    //     if (track === trackWithMostSessions) {
-    //         continue;
-    //     }
-    //
-    //     for (let i = 0; i < trackWithMostSessions.children.length; i++) {
-    //         const templateSession = trackWithMostSessions.children[i]!;
-    //         const session = track.children[i];
-    //
-    //         if (templateSession.route.path === session?.route.path) {
-    //             continue;
-    //         }
-    //     }
-    // }
-    //
-    // const grid: string[][] = [];
-    //
-    // for (let i = 0; i < trackWithMostSessions.children.length; i++) {
-    //     grid.push([]);
-    //     for (let j = 0; j < tracksWithAllSessions.length; j++) {
-    //         const session = tracksWithAllSessions[j]?.children[i];
-    //         grid[i]![j] = session?.route.path ?? 'none';
-    //     }
-    // }
-
-    return (
-        <section>
-            <SectionTitleBar title="Schedule" />
-
-            <div>
-                {tracks.map((track) => (
-                    <p key={track.id}>{track.name}</p>
-                ))}
-            </div>
-
-            <pre>
-                {grid
-                    .map((row) => row.map((col) => col.padEnd(100)).join(' '))
-                    .join('\n')}
-            </pre>
-        </section>
-    );
-}
-
-function getSessionDuration(session: SessionWithDates): number {
-    return (
-        (session.properties.end?.getTime() ?? 0) -
-        (session.properties.start?.getTime() ?? 0)
-    );
-}
-
-function sessionSort(a?: SessionWithDates, b?: SessionWithDates): number {
-    return (
-        (a?.properties.start?.getTime() ?? 0) -
-        (b?.properties.start?.getTime() ?? 0)
-    );
+    return sessionGrid;
 }
