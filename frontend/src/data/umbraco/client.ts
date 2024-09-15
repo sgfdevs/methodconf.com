e3f10f9d653b3244d0bcdd406fcf80c4454fdf3e
Levi Zitting <me@levizitting.com>
2024-09-15 12:54:42 -0500

    feat: add caching

--- frontend/src/data/umbraco/client.ts
+++ frontend/src/data/umbraco/client.ts
@@ -6,7 +6,7 @@ export const umbracoClient = createClient<paths>({
     baseUrl: NEXT_PUBLIC_UMBRACO_BASE_URL.toString(),
     fetch: async (request) => {
         try {
-            return await fetch(request, { cache: 'no-cache' });
+            return await fetch(request, { next: { revalidate: 60 } });
         } catch (err) {
             console.log(err);
             throw err;
