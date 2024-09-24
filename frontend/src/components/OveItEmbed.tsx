f7bc0cea340e476cf198f30417978a32d19444bd
Levi Zitting <me@levizitting.com>
2024-09-23 21:52:55 -0500

    fix: issue with paypal checkout

--- frontend/src/components/OveItEmbed.tsx
+++ frontend/src/components/OveItEmbed.tsx
@@ -1,14 +1,18 @@
 'use client';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { parseUrl } from '@/util';
+import { usePathname, useRouter } from 'next/navigation';
 
 export interface InlineScriptProps {
     embedUrl: string;
 }
 
 export function OveItEmbed({ embedUrl }: InlineScriptProps) {
+    const router = useRouter();
+    const pathname = usePathname();
     const iframeRef = useRef<HTMLIFrameElement>(null);
-    const parsedUrl = parseUrl(embedUrl);
+    const [iframeUrl, setIframeUrl] = useState<URL | undefined>();
+    const [embedId, setEmbedId] = useState<string | undefined>();
 
     const sendInfo = useCallback(
         (time: number) => {
@@ -16,13 +20,13 @@ export function OveItEmbed({ embedUrl }: InlineScriptProps) {
                 {
                     type: 'info',
                     referrer: window.location.href,
-                    embedID: parsedUrl?.searchParams.get('id'),
+                    embedID: embedId,
                     time: time,
                 },
                 '*',
             );
         },
-        [parsedUrl?.searchParams],
+        [embedId],
     );
 
     const resizeIframe = useCallback((dimensions: { height: number }) => {
@@ -35,6 +39,30 @@ export function OveItEmbed({ embedUrl }: InlineScriptProps) {
         }
         iframeRef.current.style.height = dimensions.height + 'px';
     }, []);
+
+    const updateIframeUrl = useCallback(() => {
+        const parsedUrl = parseUrl(embedUrl);
+
+        if (!parsedUrl) {
+            return;
+        }
+
+        const parsedEmbedId = parsedUrl?.searchParams.get('id') ?? undefined;
+
+        // Handles incoming redirects from certain payment providers
+        if (window.location.hash) {
+            const hash = window.location.hash.substring(1);
+            const parts = hash.split('/');
+
+            if (parts.length >= 2 && parts[0] === parsedEmbedId && parts[1]) {
+                parsedUrl.searchParams.set('next', parts[1]);
+                router.replace(pathname, { scroll: false });
+            }
+        }
+
+        setEmbedId(parsedEmbedId);
+        setIframeUrl(parsedUrl);
+    }, [embedUrl, pathname, router]);
 
     useEffect(() => {
         function onWindowMessage(event: MessageEvent) {
@@ -62,17 +90,21 @@ export function OveItEmbed({ embedUrl }: InlineScriptProps) {
         window.addEventListener('message', onWindowMessage);
         window.addEventListener('resize', onWindowResize);
 
+        updateIframeUrl();
+
         return () => {
             window.removeEventListener('message', onWindowMessage);
             window.removeEventListener('resize', onWindowResize);
         };
-    }, [resizeIframe, sendInfo]);
+    }, [resizeIframe, sendInfo, updateIframeUrl]);
 
-    return (
+    return iframeUrl ? (
         <iframe
             className="w-full overflow-hidden"
             ref={iframeRef}
-            src={embedUrl}
+            src={iframeUrl.toString()}
         />
+    ) : (
+        <>Loading...</>
     );
 }
