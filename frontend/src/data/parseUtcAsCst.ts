33d6e10d8c880debf851c9f047a0619642944267
Levi Zitting <me@levizitting.com>
2024-10-07 12:33:34 -0500

    fix: issue with outputting timezone times (#30)

--- frontend/src/data/parseUtcAsCst.ts
+++ frontend/src/data/parseUtcAsCst.ts
@@ -3,7 +3,7 @@ import { CST_TZ } from '@/config';
 import { TZDate } from '@date-fns/tz';
 
 // Umbraco doesn't save dates with timezone information so we have to manually fix it
-export function parseUtcAsCst(dateStr: string): Date {
+export function parseUtcAsCst(dateStr: string): TZDate {
     const utcDate = parse(dateStr, "yyyy-MM-dd'T'HH:mm:ss'Z'", new Date());
 
     return new TZDate(utcDate, CST_TZ);
