e66d094caf64e14e453f11c9f638c6e3e4b00476
Levi Zitting <me@levizitting.com>
2024-10-07 13:57:34 -0500

    fix: issues when host is in CST

--- frontend/src/data/parseUtcAsCst.ts
+++ frontend/src/data/parseUtcAsCst.ts
@@ -1,18 +1,19 @@
-import { CST_TZ } from '@/config';
+import { parse } from 'date-fns';
 import { TZDate } from '@date-fns/tz';
+import { CST_TZ } from '@/config';
 
 // Umbraco doesn't save dates with timezone information so we have to manually fix it
 export function parseUtcAsCst(dateStr: string): Date {
-    const date = new Date(dateStr);
+    const utcDate = parse(dateStr, "yyyy-MM-dd'T'HH:mm:ss'Z'", new Date());
 
     return new TZDate(
-        date.getFullYear(),
-        date.getMonth(),
-        date.getDate(),
-        date.getHours(),
-        date.getMinutes(),
-        date.getSeconds(),
-        date.getMilliseconds(),
+        utcDate.getFullYear(),
+        utcDate.getMonth(),
+        utcDate.getDate(),
+        utcDate.getHours(),
+        utcDate.getMinutes(),
+        utcDate.getSeconds(),
+        utcDate.getMilliseconds(),
         CST_TZ,
     );
 }
