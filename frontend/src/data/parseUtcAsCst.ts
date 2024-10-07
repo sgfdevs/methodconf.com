4f50f9592dcf121229a5c3da5638beb5ae2927fd
Levi Zitting <me@levizitting.com>
2024-10-07 12:42:45 -0500

    fix: remove utc from date string in parsing (#31)

--- frontend/src/data/parseUtcAsCst.ts
+++ frontend/src/data/parseUtcAsCst.ts
@@ -1,10 +1,9 @@
-import { parse } from 'date-fns';
 import { CST_TZ } from '@/config';
 import { TZDate } from '@date-fns/tz';
 
 // Umbraco doesn't save dates with timezone information so we have to manually fix it
 export function parseUtcAsCst(dateStr: string): TZDate {
-    const utcDate = parse(dateStr, "yyyy-MM-dd'T'HH:mm:ss'Z'", new Date());
+    const dateStrWithoutZ = dateStr.replace(/Z$/, '');
 
-    return new TZDate(utcDate, CST_TZ);
+    return new TZDate(dateStrWithoutZ, CST_TZ);
 }
