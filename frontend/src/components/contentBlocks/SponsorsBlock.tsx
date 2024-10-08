920c42c68d7a94de983e539b985030d43ce578eb
Levi Zitting <me@levizitting.com>
2024-10-08 10:51:22 -0500

    feat: update fe to use new opp url

--- frontend/src/components/contentBlocks/SponsorsBlock.tsx
+++ frontend/src/components/contentBlocks/SponsorsBlock.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import type { Sponsors } from '@/data/types';
 import logo from '../../../public/method-logo.svg';
 import { SponsorTierItem } from '@/components/SponsorTierItem';
+import { Button } from '@/components/Button';
 
 export interface SponsorBlockProps {
     sponsors?: Sponsors;
@@ -13,6 +14,8 @@ export function SponsorsBlock({ sponsors }: SponsorBlockProps) {
         sponsors?.properties?.tiers?.items
             .map((item) => item.content)
             .filter((content) => content.contentType === 'sponsorTier') ?? [];
+
+    const opportunitiesUrl = sponsors?.properties?.opportunitiesUrl?.[0];
 
     return (
         <section id="sponsor" className="mb-12 sm:mb-20">
@@ -30,25 +33,23 @@ export function SponsorsBlock({ sponsors }: SponsorBlockProps) {
                     </div>
                 ) : null}
 
-                <div className="content-container bg-black relative overflow-clip">
-                    <div className="bg-primary w-0 sm:w-1/4 lg:w-1/3 h-full absolute right-0 top-0 bottom-0"></div>
-                    <div className="bg-primary w-[200%] sm:w-full h-[200%] absolute right-0 sm:right-1/4 lg:right-1/3 top-0 bottom-0 -rotate-45 origin-top-right"></div>
-                    <div className="flex flex-col xl:flex-row justify-center relative py-20 px-10 xl:space-x-8 items-center space-y-4 xl:space-y-0">
-                        <Image src={logo} alt="Method Logo" />
+                {opportunitiesUrl ? (
+                    <div className="content-container bg-black relative overflow-clip">
+                        <div className="bg-primary w-0 sm:w-1/4 lg:w-1/3 h-full absolute right-0 top-0 bottom-0"></div>
+                        <div className="bg-primary w-[200%] sm:w-full h-[200%] absolute right-0 sm:right-1/4 lg:right-1/3 top-0 bottom-0 -rotate-45 origin-top-right"></div>
+                        <div className="flex flex-col xl:flex-row justify-center relative py-20 px-10 xl:space-x-8 items-center space-y-4 xl:space-y-0">
+                            <Image src={logo} alt="Method Logo" />
 
-                        <p className="text-white font-bold text-2xl text-center">
-                            Interested in becoming a sponsor?
-                        </p>
-
-                        <a
-                            target="_blank"
-                            href="/2024-Method-Conference-Sponsorship-Packet.pdf"
-                            className="button inline-block"
-                        >
-                            See Opportunities
-                        </a>
+                            <p className="text-white font-bold text-2xl text-center">
+                                Interested in becoming a sponsor?
+                            </p>
+                            <Button
+                                className="inline-block"
+                                cmsLink={opportunitiesUrl}
+                            />
+                        </div>
                     </div>
-                </div>
+                ) : null}
             </div>
         </section>
     );
