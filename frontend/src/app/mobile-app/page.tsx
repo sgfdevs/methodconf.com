7ea64033aeba65840f343f55ad6c901bec6d4eaf
Levi Zitting <me@levizitting.com>
2024-10-10 19:27:51 -0500

    feat: mobile app redirect (#42)
    * feat: add mobile app redirect
    
    * fix: styling on client page

---
+++ frontend/src/app/mobile-app/page.tsx
@@ -0,0 +1,27 @@
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { APPLE_APP_STORE_LINK, GOOGLE_PLAY_STORE_LINK } from '@/config';
+import { ClientMobileAppRedirect } from '@/components/ClientMobileAppRedirect';
+
+export default function MobileApp() {
+    const headersList = headers();
+    const userAgent = headersList.get('user-agent');
+
+    if (!userAgent) {
+        return redirect('/');
+    }
+
+    if (/android/i.test(userAgent)) {
+        return redirect(GOOGLE_PLAY_STORE_LINK);
+    }
+
+    if (/iPhone|iPad|iPod/i.test(userAgent)) {
+        return redirect(APPLE_APP_STORE_LINK);
+    }
+
+    return (
+        <div className="content-container">
+            <ClientMobileAppRedirect />
+        </div>
+    );
+}
